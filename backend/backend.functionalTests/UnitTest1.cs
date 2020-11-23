dee0faf263872adc61b707df581d94ed8f893cd6
Phanlop P <a.pengsupan@gmail.com>
2020-11-23 04:35:22 +0000

    init functional test project

---
+++ backend/backend.functionalTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace backend.functionalTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
